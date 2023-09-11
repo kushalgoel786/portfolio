7bc502ceeda05b8e4f49d25def3120ceb04a692b
Kushal Goel <kushalgoel786@gmail.com>
2023-09-11 13:06:50 -0400

    Fixed a bug in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,21 +5,21 @@ const Footer = () => {
         Built with
         <a
           href="https://nextjs.org/"
-          class="font-medium text-slate-400 hover:text-teal-300"
+          className="font-medium text-slate-400 hover:text-teal-300"
           target="_blank">
           &nbsp;Next.js&nbsp;
         </a>
         and
         <a
           href="https://tailwindcss.com/"
-          class="font-medium text-slate-400 hover:text-teal-300"
+          className="font-medium text-slate-400 hover:text-teal-300"
           target="_blank">
           &nbsp;Tailwind CSS
         </a>
         , deployed using
         <a
           href="https://vercel.com/"
-          class="font-medium text-slate-400 hover:text-teal-300"
+          className="font-medium text-slate-400 hover:text-teal-300"
           target="_blank">
           &nbsp;Vercel
         </a>
@@ -27,21 +27,21 @@ const Footer = () => {
         <a
           target="_blank"
           href="https://code.visualstudio.com/"
-          class="font-medium text-slate-400 hover:text-teal-300">
+          className="font-medium text-slate-400 hover:text-teal-300">
           &nbsp;Visual Studio Code
         </a>
         . All text is set in the
         <a
           href="https://rsms.me/inter/"
           target="_blank"
-          class="font-medium text-slate-400 hover:text-teal-300">
+          className="font-medium text-slate-400 hover:text-teal-300">
           &nbsp;Inter&nbsp;
         </a>
         typeface. Inspired by Brittany Chiang&apos;s
         <a
           href="https://brittanychiang.com/"
           target="_blank"
-          class="font-medium text-slate-400 hover:text-teal-300">
+          className="font-medium text-slate-400 hover:text-teal-300">
           &nbsp;portfolio
         </a>
         .
